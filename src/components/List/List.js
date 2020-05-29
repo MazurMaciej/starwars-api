3ab00c9ec4fb2d5c467d527feb6f22f0a13248d4
mr.mazurmaciej@gmail.com <mr.mazurmaciej@gmail.com>
2020-05-29 14:51:09 +0200

    change the structure of app from Hooks on Class Component with ComponentDidUpdate

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -36,41 +36,89 @@ const WrapperPage = styled.div`
   justify-content: space-between;
 `;
 
-const List = () => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setIsError] = useState(false);
+// const List = () => {
+//   const [data, setData] = useState([]);
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setIsError] = useState(false);
 
-  useEffect(() => {
-    fetch('https://swapi.co/api/people/')
+//   useEffect(() => {
+//     fetch('https://swapi.dev/api/people/')
+//       .then(response => response.json())
+//       .then(response => {
+//         setData(response.results)
+//         setIsLoading(false);
+//       })
+//       .catch(error => {
+//         setIsError(true);
+//         console.log(`Content is available, error: ${error}`);
+//       });
+//   }, [data])
+
+//   return (
+// <WrapperPage>
+//   <PageTitle>starwars - heroes</PageTitle>
+//   {error ? (<Statment>Sorry, content is available, try again later . . .</Statment>) : (isLoading && <Statment>Wait,  I'm loading StarWars heroes for you . . . </Statment>)}
+//   {data.map(({ name, birth_year, gender, hair_color, eye_color, height, mass }) =>
+//     <Item
+//       key={name}
+//       name={name}
+//       birth={birth_year}
+//       gender={gender}
+//       height={height}
+//       hair={hair_color}
+//       eye={eye_color}
+//       mass={mass}
+//     />)}
+// </WrapperPage >
+//   )
+// };
+
+// export default List
+
+class List extends React.Component {
+  state = {
+    data: [],
+    error: false,
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    fetch('https://swapi.dev/api/people/')
       .then(response => response.json())
       .then(response => {
-        setData(response.results)
-        setIsLoading(false);
+        this.setState({
+          data: response.results,
+          isLoading: false,
+        });
+
       })
       .catch(error => {
-        setIsError(true);
+        this.setState({
+          error: true
+        });
         console.log(`Content is available, error: ${error}`);
       });
-  }, [data])
+  }
 
-  return (
-    <WrapperPage>
-      <PageTitle>starwars - heroes</PageTitle>
-      {error ? (<Statment>Sorry, content is available, try again later . . .</Statment>) : (isLoading && <Statment>Wait,  I'm loading StarWars heroes for you . . . </Statment>)}
-      {data.map(({ name, birth_year, gender, hair_color, eye_color, height, mass }) =>
-        <Item
-          key={name}
-          name={name}
-          birth={birth_year}
-          gender={gender}
-          height={height}
-          hair={hair_color}
-          eye={eye_color}
-          mass={mass}
-        />)}
-    </WrapperPage >
-  )
+  render() {
+    return (
+      <WrapperPage>
+        <PageTitle>starwars - heroes</PageTitle>
+        {this.state.error ? (<Statment>Sorry, content is available, try again later . . .</Statment>) : (this.state.isLoading && <Statment>Wait,  I'm loading StarWars heroes for you . . . </Statment>)}
+        {this.state.data.map(({ name, birth_year, gender, hair_color, eye_color, height, mass }) =>
+          <Item
+            key={name}
+            name={name}
+            birth={birth_year}
+            gender={gender}
+            height={height}
+            hair={hair_color}
+            eye={eye_color}
+            mass={mass}
+          />)}
+      </WrapperPage >
+    )
+  }
 };
 
-export default List
+export default List;
